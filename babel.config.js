1d7d6c23866ad386ea0b78a86beaf2bdda0aff70
Kino_saur <kaungkhantlin999@gmail.com>
2025-08-27 10:32:44 +0700

    Update babel.config.js
    fix: remove 'react-native-reanimated/plugin' from babel.config.js plugins

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,6 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       ['react-native-worklets-core/plugin'],
-      // Reanimated plugin MUST be last
-      'react-native-reanimated/plugin',
     ],
   }
 }
